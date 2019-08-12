067999c4294350047b2d63a13cbbbff4535ce402
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-12 15:35:58 +0300

    burger instantiate

---
+++ Matryoshka_Task/Assets/Scripts/BunClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunClick : MonoBehaviour
+{
+    [SerializeField] GameObject bun = null;
+    [SerializeField] GameObject meat = null;
+    [SerializeField] Transform burgerpoint;
+
+    GameObject buns;
+
+    private void OnMouseDown()
+    {
+        //look for an empty desk
+        //if there is an emty desk we need to take it
+        //instantiate
+        //if there is not do nothing
+        buns=Instantiate(bun, burgerpoint.position, burgerpoint.rotation);
+        Debug.Log("Click");
+    }
+
+    public void Delete()
+    {
+        Destroy(buns);
+    }
+
+}
---
+++ Matryoshka_Task/Assets/Scripts/Meat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meat : MonoBehaviour
+{
+    private bool isReady = true;
+
+    private void OnMouseDown()
+    {
+        if(isReady)
+        {
+            FindObjectOfType<BurgerDesk>().GetMeat();
+        }
+
+        //if it is ready we can remove on the bun
+        //if there is an empty bun take it
+        //if there is not do nothing;
+    }
+}
