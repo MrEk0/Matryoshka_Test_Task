1295551ebe16743e1a9022256dd013153144cc64
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-09 22:19:50 +0300

    spawn food on desks

---
+++ Matryoshka_Task/Assets/Scripts/FoodBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBox : MonoBehaviour
+{
+    [SerializeField] GameObject FoodPrefab;
+    [SerializeField] List<Transform> FoodPoints;
+
+    private void Awake()//to make the toggle always true at the beginning
+    {
+        foreach (Transform point in FoodPoints)
+        {
+            point.GetComponent<FoodPoint>().isEmpty = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //look for a place
+        GameObject place= LookForAPlace();
+        if (place!=null)
+        {
+            /*GameObject meat = */Instantiate(FoodPrefab, place.transform.position, place.transform.rotation);
+            place.GetComponent<FoodPoint>().isEmpty = false;
+            //start cook
+        }
+    }
+
+    private GameObject LookForAPlace()
+    {
+        foreach (Transform point in FoodPoints)
+        {
+            if (point.GetComponent<FoodPoint>().isEmpty==true)
+            {
+                return point.gameObject;
+            }
+            //Debug.Log(point.GetComponent<MeatPoint>().isEmpty+"  isempty");
+        }
+
+        return null;
+    }
+}
--- Matryoshka_Task/Assets/Scripts/FoodPoint.cs
+++ Matryoshka_Task/Assets/Scripts/FoodPoint.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class MeatPoint : MonoBehaviour
+public class FoodPoint : MonoBehaviour
 {
     public bool isEmpty { get; set; } = true;
 
